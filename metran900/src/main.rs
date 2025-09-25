4ebc56c0d14a83c996e40ea43c58ff8b31a9f307
Vyacheslav Kononov <kononov.va.arh@yandex.ru>
2025-09-25 21:50:32 +0300

    2025 09 25 night

--- metran900/src/main.rs
+++ metran900/src/main.rs
@@ -1,5 +1,11 @@
 use std::{fs::OpenOptions, io::{Read, Write}};
 use crc16::*;
+use chrono::{DateTime, Local, TimeZone, Utc};
+
+struct DeviceAnswer{
+    message_time: DateTime<Local>,
+    message_items: [f32; 12],
+}
 
 fn main() {
     let message: [u8; 7] = [1, 0x55, 0x4d, 0xff, 0xff, 0, 0];
@@ -10,21 +16,34 @@ fn main() {
     file_desc.write(&message).expect("failed to write ttyS3");
     file_desc.read(&mut answer).expect("failed to read ttyS3");
 
-    let message_crc16 = State::<AUG_CCITT>::calculate(&answer[0..30]);
+    let f_answer = get_format_message(&answer).expect("CRC error");
 
-    for item in answer
-    {
-        print!("{:X} ", item);
+    print!("{}; ", f_answer.message_time);
+    for item in f_answer.message_items{
+        print!("{}; ", item);
     }
-    println!("\nAUG_CCITT {:X}", message_crc16);
-    println!("CCITT_FALSE {:X}", State::<CCITT_FALSE>::calculate(&answer[0..30]));
-    println!("CRC_A {:X}", State::<CRC_A>::calculate(&answer[0..30]));
-    println!("DDS_110 {:X}", State::<DDS_110>::calculate(&answer[0..30]));
-    println!("GENIBUS {:X}", State::<GENIBUS>::calculate(&answer[0..30]));
-    println!("KERMIT {:X}", State::<KERMIT>::calculate(&answer[0..30]));
-    println!("MCRF4XX {:X}", State::<MCRF4XX>::calculate(&answer[0..30]));
-    println!("RIELLO {:X}", State::<RIELLO>::calculate(&answer[0..30]));
-    println!("TMS37157 {:X}", State::<TMS37157>::calculate(&answer[0..30]));
-    println!("XMODEM {:X}", State::<XMODEM>::calculate(&answer[0..30]));
-    println!("X_25 {:X}", State::<X_25>::calculate(&answer[0..30]));
+}
+
+fn get_format_message(buf: &[u8]) -> Result<DeviceAnswer, &'static str>{
+    if u16::from_le_bytes([buf[31], buf[32]]) == State::<XMODEM>::calculate(&buf[0..31]) {
+        let formated_answer = DeviceAnswer{
+            message_time: Local::now(),
+            message_items: [f32::from(u16::from_le_bytes([buf[5], buf[6]]))/32.0, 
+                f32::from(u16::from_le_bytes([buf[7], buf[8]]))/32.0, 
+                f32::from(u16::from_le_bytes([buf[9], buf[10]]))/32.0, 
+                f32::from(u16::from_le_bytes([buf[11], buf[12]]))/32.0, 
+                f32::from(u16::from_le_bytes([buf[13], buf[14]]))/32.0, 
+                f32::from(u16::from_le_bytes([buf[15], buf[16]]))/32.0, 
+                f32::from(u16::from_le_bytes([buf[17], buf[18]]))/32.0, 
+                f32::from(u16::from_le_bytes([buf[19], buf[20]]))/32.0, 
+                f32::from(u16::from_le_bytes([buf[21], buf[22]]))/32.0, 
+                f32::from(u16::from_le_bytes([buf[23], buf[24]]))/32.0, 
+                f32::from(u16::from_le_bytes([buf[25], buf[26]]))/32.0, 
+                f32::from(u16::from_le_bytes([buf[27], buf[28]]))/32.0],
+        };
+        Ok(formated_answer)
+    }
+    else {
+        Err("CRC error")
+    }
 }
